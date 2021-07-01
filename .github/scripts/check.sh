3f9eff6162aeef39a08f2ade922a18c4cebceeb4
ggzor <30713864+ggzor@users.noreply.github.com>
2021-07-01 17:04:04 -0500

    Updating workflow

---
+++ .github/scripts/check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Checking some vars:"
+echo "WORKFLOW_VAR='$WORKFLOW_VAR'"
+echo "JOB_VAR='$JOB_VAR'"
+echo "MATRIX_VAR='$MATRIX_VAR'"
+echo "STEP_VAR='$STEP_VAR'"
+echo "SECRET_VAR='$SECRET_VAR'"
+
+echo "Generating test artifact..."
+
+printf "
+Some generated test data:
+MATRIX = %s
+" "$MATRIX_VAR" > test-output.txt
+
+echo "Some environment data:
+pwd=$(pwd)
+ls -al=
+$(ls -al)
+"
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Testing GitHub actions
+
+![Build badge](https://github.com/ggzor/github-actions-test1/actions/workflows/build.yml/badge.svg)
+
+Testing some GitHub actions
